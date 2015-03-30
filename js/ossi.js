284fb39df6cc00cc0d70aeb0377964dbdc3a052c
Keith Funkhouser <keith.funkhouser@gmail.com>
2015-03-30 11:49:33 -0500

    complete

--- js/ossi.js
+++ js/ossi.js
@@ -39,34 +39,25 @@ itemSelector: '.element-item',
     columnWidth: 300,
   });
 
-  // filter functions
-  var filterFns = {
-    // show if number is greater than 50
-    numberGreaterThan50: function() {
-      var number = $(this).find('.number').text();
-      return parseInt( number, 10 ) > 50;
-    },
-    // show if name ends with -ium
-    ium: function() {
-      var name = $(this).find('.name').text();
-      return name.match( /ium$/ );
-    },
-    hasImg: function() {
-      var img = $(this).find('.Image_URL');
-      //img[0].src contains the URL
-      return img.length != 0;
-    }
-  };
+  // store filter for each group
+  var filters = {};
 
-  // bind filter button click
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
+  $('#filters').on( 'click', '.button', function() {
+    var $this = $(this);
+    // get group key
+    var $buttonGroup = $this.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $this.attr('data-filter');
+    // combine filters
+    var filterValue = '';
+    for ( var prop in filters ) {
+      filterValue += filters[ prop ];
+    }
+    // set filter for Isotope
     $container.isotope({ filter: filterValue });
   });
 
-  
   // change is-checked class on buttons
   $('.button-group').each( function( i, buttonGroup ) {
     var $buttonGroup = $( buttonGroup );
@@ -214,7 +205,8 @@ function createP(className, text) {
 }
 
 function escapeClassName(className) {
-  return( className.replace(/ /g, "_") );
+  className = className.replace(/ /g, "_");
+  return( className.replace(/,/g, "_") );
 }
 
 function makeRowDiv(result, idx) {
